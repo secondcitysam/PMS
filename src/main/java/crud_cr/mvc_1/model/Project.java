6724d7fd3f0aa4a0235bc4b3a519b9e226f70178
secondcitysam <samyak.kedari4444@gmail.com>
2025-12-28 21:50:05 +0530

    v3

---
+++ src/main/java/crud_cr/mvc_1/model/Project.java
@@ -0,0 +1,23 @@
+package crud_cr.mvc_1.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
---
+++ src/main/java/crud_cr/mvc_1/model/ProjectStatus.java
@@ -0,0 +1,8 @@
+package crud_cr.mvc_1.model;
+
+public enum ProjectStatus {
+
+    PLANNED,
+    IN_PROGRESS,
+    COMPLETED
+}
